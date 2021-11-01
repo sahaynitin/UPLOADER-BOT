ef9f06355f00dbc884794a8f92602056b4dfa66c
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-01 15:07:15 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,42 +1,36 @@
 class Translation(object):
-    START_TEXT = """𝐇𝐞𝐥𝐥𝐨! 🙋
+    START_TEXT = """Hey {}
 
-𝐈 𝐚𝐦 𝐀 𝐏𝐨𝐰𝐞𝐫𝐅𝐮𝐥 𝐔𝐫𝐥 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐫 𝐁𝐨𝐭 𝐭𝐡𝐚𝐭 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐬 𝐆𝐝𝐫𝐢𝐯𝐞 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫 𝐒𝐢𝐭𝐞𝐬 𝐋𝐢𝐧𝐤... 𝐖𝐢𝐭𝐡 𝐇𝐨𝐭𝐬𝐭𝐚𝐫. 
+I am Url Uploader Bot 
 
-𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧 𝐔𝐩𝐥𝐨𝐚𝐝 𝐕𝐢𝐝𝐞𝐨/𝐅𝐢𝐥𝐞 𝐅𝐨𝐫𝐦𝐚𝐭 𝐭𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐰𝐢𝐭𝐡 𝐀𝐝𝐝𝐞𝐝 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐒𝐮𝐩𝐩𝐨𝐫𝐭.
+I can Upload Any Link in Media Form
 
-/help 𝐟𝐨𝐫 𝐦𝐨𝐫𝐞 𝐝𝐞𝐭𝐚𝐢𝐥𝐬!
+Use Help Command to Know How to Use me.
 
-𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 :@TheTeleRoid ❤
+Made With 💕 By @Tellybots_4u
 """
 
-    HELP_USER = """𝐈𝐭'𝐬 𝐧𝐨𝐭 𝐭𝐡𝐚𝐭 𝐜𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝 𝐭𝐨 𝐮𝐬𝐞 𝐦𝐞𝐡! 😅
+    HELP_USER = """Welcome to Help Menu
     
-✪ 𝐒𝐞𝐧𝐝 𝐌𝐞 𝐀 𝐓𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐢𝐟 𝐫𝐞𝐪𝐮𝐢𝐫𝐞𝐝. 𝐈𝐭'𝐥𝐥 𝐛𝐞 𝐬𝐚𝐯𝐞𝐝 𝐩𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭𝐥𝐲.💯
-✪ 𝐈𝐟 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐖𝐚𝐬𝐧'𝐭 𝐒𝐞𝐭 𝐁𝐲 𝐘𝐨𝐮, 𝐈𝐭'𝐥𝐥 𝐁𝐞 𝐀𝐮𝐭𝐨 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐅𝐫𝐨𝐦 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞.🥳
-✪ 𝐒𝐞𝐧𝐝 𝐌𝐞 𝐀𝐧𝐲 𝐋𝐢𝐧𝐤 𝐓𝐨 𝐁𝐞 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐓𝐨 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦.
-✪ 𝐏𝐫𝐞𝐬𝐬 /𝐝𝐞𝐥𝐞𝐭𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐓𝐨 𝐃𝐞𝐥𝐞𝐭𝐞 𝐘𝐨𝐮𝐫 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥.
-✪ 𝐒𝐞𝐥𝐞𝐜𝐭 𝐭𝐡𝐞 𝐛𝐮𝐭𝐭𝐨𝐧.
-   𝐒𝐕𝐢𝐝𝐞𝐨 - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐕𝐢𝐝𝐞𝐨  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐚𝐬 𝐯𝐢𝐝𝐞𝐨 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-   𝐃𝐅𝐢𝐥𝐞  - 𝐆𝐢𝐯𝐞 𝐅𝐢𝐥𝐞 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐒𝐜𝐫𝐞𝐞𝐧𝐬𝐡𝐨𝐭𝐬
-𝐍𝐁 : 𝐈𝐭 𝐢𝐬 𝐑𝐞𝐜𝐨𝐦𝐦𝐞𝐧𝐝𝐞𝐝 𝐓𝐨 𝐔𝐬𝐞 𝐀 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐁𝐞𝐜𝐚𝐮𝐬𝐞, 𝐒𝐨𝐦𝐞 𝐓𝐢𝐦𝐞 𝐖𝐨𝐧'𝐭 𝐔𝐩𝐥𝐨𝐚𝐝 𝐓𝐡𝐞 𝐅𝐢𝐥𝐞 𝐖𝐢𝐭𝐡𝐨𝐮𝐭 𝐚 𝐂𝐮𝐬𝐭𝐨𝐦 𝐓𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥.
-𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩 : @TeleRoid14
+✪ Send a Custom Thumbnail to Save Thumbnail\n
+✪ Please Send Any Link to Upload on Telegram.\n
+✪ Use /delthumbnail to Delete Your Custom Thumbnail\n
+✪ Use Any Button Below on Sending link\n
+Its Recommended to Use A Custom Thumbnail Before Sending link
 """
     ABOUT_TEXT = """<b>🤖 𝐌𝐲 𝐍𝐚𝐦𝐞 : <a href='https://t.me/TeleRoid_Hotstar_Bot'> @𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝_𝐇𝐨𝐭𝐬𝐭𝐚𝐫_𝐁𝐨𝐭 </a></b>
 
-<b>👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫:<a href='https://t.me/PredatorHackerzZ'>@𝐏𝐫𝐞𝐝𝐚𝐭𝐨𝐫𝐇𝐚𝐜𝐤𝐞𝐫𝐳𝐙</a></b>
+<b>👨‍💻 My Name:<a href='https://t.me/PredatorHackerzZ'>@𝐏𝐫𝐞𝐝𝐚𝐭𝐨𝐫𝐇𝐚𝐜𝐤𝐞𝐫𝐳𝐙</a></b>
 
-<b>📝 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: 𝐏𝐲𝐭𝐡𝐨𝐧𝟑</b>
+<b>📝 language: Python3</b>
 
-<b>📡 𝐒𝐞𝐫𝐯𝐞𝐫: <a href='https://www.heroku.com'> 𝐇𝐞𝐫𝐨𝐤𝐮 </a></b>
+<b>📡 Server :<a href='https://www.heroku.com'> Heroku </a></b>
 
-<b>📕 𝐋𝐢𝐛𝐫𝐚𝐫𝐲: Pyrogram 1.0.7</b>
+<b>📚 library : Pyrogram 1.0.7</b>
 
-<b>👥 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞:<a href='https://github.com/PredatorHackerzZ/UPLOADER-BOT'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a></b>
+<b>📁 Source Code :<a href='https://github.com/PredatorHackerzZ/UPLOADER-BOT'>𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a></b>
 
-<b>📢 𝐁𝐨𝐭𝐬 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :<a href='https://t.me/TeleRoid14'>@𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝𝐒𝐮𝐩𝐩𝐨𝐫𝐭</a></b>
+<b>🤖 Update Channel :<a href='https://t.me/TeleRoid14'>@𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝𝐒𝐮𝐩𝐩𝐨𝐫𝐭</a></b>
 
 <b>📢 𝐁𝐨𝐭𝐬 𝐔𝐩𝐝𝐚𝐭𝐞𝐬: <a href='https://t.me/TeleRoidGroup'>@𝐓𝐞𝐥𝐞𝐑𝐨𝐢𝐝𝐆𝐫𝐨𝐮𝐩</a></b>
 
@@ -59,6 +53,6 @@ URL | filename | username | password"""
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """𝐌𝐞𝐝𝐢𝐚 𝐀𝐥𝐛𝐮𝐦 𝐬𝐡𝐨𝐮𝐥𝐝 𝐜𝐨𝐧𝐭𝐚𝐢𝐧 𝐨𝐧𝐥𝐲 𝐭𝐰𝐨 𝐩𝐡𝐨𝐭𝐨𝐬. 𝐏𝐥𝐞𝐚𝐬𝐞 𝐫𝐞-𝐬𝐞𝐧𝐝 𝐭𝐡𝐞 𝐦𝐞𝐝𝐢𝐚 𝐚𝐥𝐛𝐮𝐦, 𝐚𝐧𝐝 𝐭𝐡𝐞𝐧 𝐭𝐫𝐲 𝐚𝐠𝐚𝐢𝐧, 𝐨𝐫 𝐬𝐞𝐧𝐝 𝐨𝐧𝐥𝐲 𝐭𝐰𝐨 𝐩𝐡𝐨𝐭𝐨𝐬 𝐢𝐧 𝐚𝐧 𝐚𝐥𝐛𝐮𝐦."
 𝐘𝐨𝐮 𝐜𝐚𝐧 𝐮𝐬𝐞 /rename 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐚𝐟𝐭𝐞𝐫 𝐫𝐞𝐜𝐞𝐢𝐯𝐢𝐧𝐠 𝐟𝐢𝐥𝐞 𝐭𝐨 𝐫𝐞𝐧𝐚𝐦𝐞 𝐢𝐭 𝐰𝐢𝐭𝐡 𝐜𝐮𝐬𝐭𝐨𝐦 𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥 𝐬𝐮𝐩𝐩𝐨𝐫𝐭.
 """
-    CANCEL_STR = "𝐏𝐫𝐨𝐜𝐞𝐬𝐬 𝐂𝐚𝐧𝐜𝐞𝐥𝐥𝐞𝐝"
+    CANCEL_STR = "Your Process Cancelled ⛔"
     ZIP_UPLOADED_STR = "𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 {} 𝐟𝐢𝐥𝐞𝐬 𝐢𝐧 {} 𝐬𝐞𝐜𝐨𝐧𝐝𝐬"
     SLOW_URL_DECED = "𝐆𝐨𝐬𝐡 𝐭𝐡𝐚𝐭 𝐬𝐞𝐞𝐦𝐬 𝐭𝐨 𝐛𝐞 𝐚 𝐯𝐞𝐫𝐲 𝐬𝐥𝐨𝐰 𝐔𝐑𝐋. 𝐒𝐢𝐧𝐜𝐞 𝐲𝐨𝐮 𝐰𝐞𝐫𝐞 𝐬𝐜𝐫𝐞𝐰𝐢𝐧𝐠 𝐦𝐲 𝐡𝐨𝐦𝐞, 𝐈 𝐚𝐦 𝐢𝐧 𝐧𝐨 𝐦𝐨𝐨𝐝 𝐭𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐭𝐡𝐢𝐬 𝐟𝐢𝐥𝐞. 𝐌𝐞𝐚𝐧𝐰𝐡𝐢𝐥𝐞, 𝐰𝐡𝐲 𝐝𝐨𝐧'𝐭 𝐲𝐨𝐮 𝐭𝐫𝐲 𝐭𝐡𝐢𝐬:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
